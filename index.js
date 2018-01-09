4a12b7df9f91147aa908abbbf7046e86309961b0
MikolajSzumigalski <m.szumigalski@op.pl>
2018-01-09 18:43:20 +0100

    slowka

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ xhr.onload = function() {
         if(xhr.status === 200) {
             var slowoObject = JSON.parse(xhr.responseText);
             var newSlowka = '';
+            newSlowka += '<table class="w3-table w3-striped w3-centered">';
             for (var i = 0; i < slowoObject.dictionary.length; i++) {
-                newSlowka += '<div>';
-                newSlowka += slowoObject.dictionary[i].slowo;
-                
+                newSlowka += '<tr><td onclick="myFunction('+"'slowo"+i+"'"+ ')">' + slowoObject.dictionary[i].slowo + "</td></tr>";
+                newSlowka += "<tr><td id='slowo"+i+"' class='w3-hide'><b>"+ slowoObject.dictionary[i].tlumaczenie + "</b><br>";    
+                newSlowka += slowoObject.dictionary[i].zdanie + "</td></tr>";  
         }
+            newSlowka += '</table>';
+            document.getElementById('slowka').innerHTML = newSlowka;
+    }
+}
+
+xhr.open('GET', 'rozdzial6.json', true);
+xhr.send(null);            
